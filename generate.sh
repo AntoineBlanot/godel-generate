ae9aa5af5441298c970ceaac8bb41de06a651646
Chikara <chikara@haru-pc.unassigned-domain>
2023-03-21 12:00:55 +0900

    generate

--- generate.sh
+++ generate.sh
@@ -1,1 +1,11 @@
-python generate.py --n 100 --knowledge "Your favorite sport is tennis." --dialog "If you had to pick one, what would your favorite sport be?"
+#!/bin/bash
+
+N=10
+OUT_FILE="data/dummy.txt"
+DEVICE="cuda"           # cpu
+MODEL_NAME="microsoft/GODEL-v1_1-base-seq2seq"      # microsoft/GODEL-v1_1-large-seq2seq
+KNOWLEDGE="Your favorite sport is tennis."          # input the knowledge here
+DIALOG="If you had to pick one, what would your favorite sport be?"     # input the dialog here
+
+python generate.py --n $N --model_name "$MODEL_NAME" --device "$DEVICE" --out "$OUT_FILE" \
+    --knowledge "$KNOWLEDGE" --dialog "$DIALOG"
